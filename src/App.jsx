38f7e764c598bde9819df9436e365526dd326713
Guilherme Teixeira Martins Oem <guilhermeteixeira3113@gmail.com>
2025-08-11 11:12:18 -0300

    chore: add space between Y

--- src/App.jsx
+++ src/App.jsx
@@ -517,13 +517,13 @@ const FleetManagementApp = () => {
                       key={expense.id}
                       className="border-b hover:bg-gray-50 text-center"
                     >
-                      <td className="px-3">
+                      <td className="px-3 py-2">
                         {new Date(expense.date).toLocaleDateString("pt-BR")}
                       </td>
-                      <td className="px-3">{vehicle?.plate}</td>
-                      <td className="px-4">
+                      <td className="px-3 py-2">{vehicle?.plate}</td>
+                      <td className="px-4 py-2">
                         <span
-                          className={`px-1 py-1 rounded text-xs ${
+                          className={`px-1 py-2 rounded text-xs ${
                             vehicle?.store === "Loja 1"
                               ? "bg-blue-100 text-blue-800"
                               : "bg-green-100 text-green-800"
@@ -532,12 +532,14 @@ const FleetManagementApp = () => {
                           {vehicle?.store}
                         </span>
                       </td>
-                      <td className="px-4">{expenseTypes[expense.type]}</td>
-                      <td className="px-3">{expense.description}</td>
-                      <td className="px-5 font-semibold">
+                      <td className="px-4 py-2">
+                        {expenseTypes[expense.type]}
+                      </td>
+                      <td className="px-3 py-2">{expense.description}</td>
+                      <td className="px-5 py-2 font-semibold">
                         R$ {expense.amount.toFixed(2)}
                       </td>
-                      <td className="px-3">
+                      <td className="px-3 py-2">
                         <div className="flex space-x-2">
                           <button
                             onClick={() => handleEditExpense(expense)}
