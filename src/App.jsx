0f0eed76ad08be44b6d3ba8069d61568ba7a590c
Guilherme Teixeira Martins Oem <guilhermeteixeira3113@gmail.com>
2025-08-11 12:07:41 -0300

    chore: add space entry columns

--- src/App.jsx
+++ src/App.jsx
@@ -294,9 +294,9 @@ const FleetManagementApp = () => {
                         <td className="px-2 font-semibold">
                           R$ {expense.amount.toFixed(2)}
                         </td>
-                        <td className="px-3">
+                        <td className="px-3 py-3">
                           <span
-                            className={`px-1 py-1 rounded text-left text-xs ${
+                            className={`px-2 py-2 rounded text-left text-xs ${
                               vehicle?.store === "Loja 1"
                                 ? "bg-blue-100 text-blue-800"
                                 : "bg-green-100 text-green-800"
